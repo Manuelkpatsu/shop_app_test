d39d4ba744032467d38095e2affc7467fee421ed
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2023-09-09 20:44:31 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'screen/home/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
